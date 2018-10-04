a265b77cb51bc379cdb601197966cdc760e10f2b
iMawe <37808370+iMawe@users.noreply.github.com>
2018-10-04 16:51:06 -0500

    pruebarb

--- Pointer Machines/RB/main.cpp
+++ Pointer Machines/RB/main.cpp
@@ -1,16 +1,26 @@
 #include "RB.h"
 #include <iostream>
 #include <fstream>
+#include <bits/stdc++.h>
+#include <time.h>
+
 using namespace std;
 
 int main(){
     RB<int, int> treerb;
+    int iSecret, jSecret;
+    srand (time(NULL));
 
-    treerb[17] = 2;
-    treerb[7] = 2;
-    treerb[90] = 2;
-    treerb[4] = 2;
-    treerb.remove(7,0);
+
+    for(int i=1;i<30;i++){
+        iSecret = rand() % 30 + 1;
+        treerb[iSecret] = i;  
+    }
+
+    for(int j=31;j<50;j++){
+        jSecret = rand() % 30 + 1;
+    	treerb.remove(jSecret, 0);
+    }
     treerb.draw();
 
 
